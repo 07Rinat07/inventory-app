abeff0a1b28c681f0e10cf0599438d5d8c33163c
Rinat Sarmuldin <ura07srr@gmail.com>
2025-12-18 12:12:56 +0500

    Install PHPUnit 11 установлен и работает

---
+++ tests/Unit/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testPhpUnitIsAlive(): void
+    {
+        self::assertTrue(true);
+    }
+}
